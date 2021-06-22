9f760595dee6187ec232c138e4d08719519613b5
okdala2 <okdala2@gmail.com>
2021-06-21 20:21:54 -0400

    Made Custom CC Button and added card selection to rootVC

---
+++ CardCalories-Programmatic/CardSelectionVC.swift
@@ -0,0 +1,31 @@
+//
+//  CardSelectionVC.swift
+//  CardCalories-Programmatic
+//
+//  Created by Dala  on 6/21/21.
+//
+
+import UIKit
+
+class CardSelectionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
