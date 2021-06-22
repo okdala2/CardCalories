f5a10843de0de00952b89fcc28260edc99ed9fa3
okdala2 <okdala2@gmail.com>
2021-06-21 20:57:42 -0400

    Added Card assets and Configured Card Layout

--- CardCalories-Programmatic/CardSelectionVC.swift
+++ CardCalories-Programmatic/CardSelectionVC.swift
@@ -8,6 +8,11 @@
 import UIKit
 
 class CardSelectionVC: UIViewController {
+    
+    let cardImageView = UIImageView()
+    let stopButton = CCButton(backgroundColor: .systemRed, title: "Stop")
+    let restartButton = CCButton(backgroundColor: .systemGreen, title: "Reset")
+    let rulesButton = CCButton(backgroundColor: .systemBlue, title: "Rules")
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,17 +20,28 @@ class CardSelectionVC: UIViewController {
         // Do any additional setup after loading the view.
         view.backgroundColor = .blue
         
+        configureUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func configureUI() {
+        configureCardImageView()
     }
-    */
+    
+    func configureCardImageView() {
+        view.addSubview(cardImageView)
+        cardImageView.translatesAutoresizingMaskIntoConstraints = false
+        cardImageView.image = UIImage(named: "AS")
+        
+        NSLayoutConstraint.activate([
+            cardImageView.widthAnchor.constraint(equalToConstant: 250),
+            cardImageView.heightAnchor.constraint(equalToConstant: 350),
+            cardImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            cardImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -75)
+        ])
+    }
+    
+    func configureStopButton() {
+        
+    }
 
 }
